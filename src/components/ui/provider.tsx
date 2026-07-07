7dbace78d984c3083d3868a712f92d0591613f09
steph <143009220+michiGonz@users.noreply.github.com>
2026-07-07 14:46:00 -0400

    fix(ui): add shared Emotion cache and wrap ChakraProvider with CacheProvider to prevent hydration style mismatch

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,14 +1,18 @@
 "use client"
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { CacheProvider } from "@emotion/react"
+import emotionCache from "@/lib/emotion-cache"
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode"
 
 export function Provider({ children, ...props }: ColorModeProviderProps) {
   return (
-    <ColorModeProvider {...props}>
-      <ChakraProvider value={defaultSystem}>
-        {children}
-      </ChakraProvider>
-    </ColorModeProvider>
+    <CacheProvider value={emotionCache}>
+      <ColorModeProvider {...props}>
+        <ChakraProvider value={defaultSystem}>
+          {children}
+        </ChakraProvider>
+      </ColorModeProvider>
+    </CacheProvider>
   )
 }
---
+++ src/lib/emotion-cache.ts
@@ -0,0 +1,5 @@
+import createCache from '@emotion/cache'
+
+const emotionCache = createCache({ key: 'css', prepend: true })
+
+export default emotionCache

a6efd1dfb8a2cf5eb798fa81ab510e9a7fb70b3f
steph <143009220+michiGonz@users.noreply.github.com>
2026-07-07 14:38:32 -0400

    fix(ui): provide defaultSystem to ChakraProvider to satisfy Chakra runtime

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode"
 
 export function Provider({ children, ...props }: ColorModeProviderProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props}>
         {children}
       </ColorModeProvider>
